b0119109dfd3a410be065ead9be3b1ab1b0828a0
ronycal <ronycalixte@gmail.com>
2026-08-19 19:58:20 -0400

    Add Ubuntu EC2 web server

--- main.tf
+++ main.tf
@@ -91,3 +91,33 @@ resource "aws_security_group" "web" {
     Project = "aws-legacy-migration"
   }
 }
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+resource "aws_instance" "web" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t3.micro"
+  subnet_id                   = aws_subnet.public.id
+  vpc_security_group_ids      = [aws_security_group.web.id]
+  associate_public_ip_address = true
+  key_name                    = "Key"
+
+  tags = {
+    Name    = "legacy-migration-web-server"
+    Project = "aws-legacy-migration"
+  }
+}
